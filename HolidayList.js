3a94cf0d6074fc453d1d9f87e7b48727362748ec
Raj1128 <118893926+Raj1128@users.noreply.github.com>
2023-08-02 18:12:56 +0530

    Add files via upload

---
+++ HolidayList.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const list =[
+    'THe beach',
+    'The mountains',
+    'Vibrant cities',
+    'Roughting it',
+];
+
+const HolidayList = () => (
+    <seaction>
+        <h1>
+            Ultimate HoliDay Destinations
+        </h1>
+        <ul>
+            {list}
+        </ul>
+    </seaction>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
